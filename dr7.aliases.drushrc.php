cd208fdff1ed29b47fd3fb250b7d0013e4099ebf
Paul Byrne <code@pfaocle.co.uk>
2015-08-06 12:52:44 +0100

    Switch to using a standard Druphpet VM

--- dr7.aliases.drushrc.php
+++ dr7.aliases.drushrc.php
@@ -4,9 +4,9 @@
  * Example alias for testing Drupal Runner.
  */
 
-$aliases['local'] = array(
-  'uri' => 'dr7.drupal.dev:8080',
-  'root' => '/webroot/dr7.drupal.dev',
-  'remote-host' => '33.33.33.10',
+$aliases['druphpet'] = array(
+  'uri' => 'dr7.druphpet.dev',
+  'root' => '/var/sites/dr7',
+  'remote-host' => '192.168.9.10',
   'remote-user' => 'vagrant',
 );
